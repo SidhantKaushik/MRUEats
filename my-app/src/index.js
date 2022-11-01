1788a9ccae6c6818631980e7ab46799cf3d9bc3b
Bchan1999 <brandon.nic99@gmail.com>
2022-10-31 19:42:56 -0600

    Removed pages folder

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import OrderPage from './Pages/OrderPage';
+import OrderPage from './Components/OrderPage';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
