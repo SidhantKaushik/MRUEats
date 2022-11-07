58d3b26248caae3a69dc92ea35bfe514410a7894
Bchan1999 <brandon.nic99@gmail.com>
2022-11-06 21:19:34 -0700

    Link to menu and changed name of resturant to home

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import OrderPage from './Components/OrderPage';
 import Header from './Components/Header'
-import Resturants from './Components/Resturants'
+import Home from './Components/Home'
 import Profile from './Components/Profile';
 import Cart from './Components/Cart'
+import ResturantOrder from './Components/ResturantOrder';
 
 function App() {
   return (
@@ -13,10 +14,11 @@ function App() {
       <div className="App">
       <Header/>
       <Routes>
-          <Route exact path='/' element={<Resturants/>}></Route>
+          <Route exact path='/' element={<Home/>}></Route>
           <Route exact path='/profile' element={<Profile/>}></Route>
           <Route exact path='/order' element={<OrderPage/>}></Route>
           <Route exact path='/cart' element={<Cart/>}></Route>
+          <Route exact path='/resturantOrder' element={<ResturantOrder/>}></Route>
       </Routes>
       
       </div>
---
+++ my-app/src/Components/ResturantOrder.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import '../styles/Home.css';
+
+const ResturantOrder = (props) => {
+    return ( 
+        <>
+            <div>
+                Menu Goes Here
+            </div>
+        </>
+    )
+}
+
+export default ResturantOrder;
