5136ec0976064673d62a3bae948e29c5f506c54b
Erika Robles <erikarobles2551@gmail.com>
2022-11-09 22:31:35 -0700

    Fixing lint code checks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import OrderPage from './Components/OrderPage';
-import Header from './Components/Header';
-import Resturants from './Components/Resturants';
-import Profile from './Components/Profile';
-import Cart from './Components/Cart';
-import AdminPage from './Components/Admin';
-import SignupPage from './Components/Signup';
+import logo from './logo.svg'
+import './App.css'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import OrderPage from './Components/OrderPage'
+import Header from './Components/Header'
+import Resturants from './Components/Resturants'
+import Profile from './Components/Profile'
+import Cart from './Components/Cart'
+import AdminPage from './Components/Admin'
+import SignupPage from './Components/Signup'
 
-function App() {
+function App () {
   return (
     <Router>
-      <div className="App">
-      <Header/>
-      <Routes>
-          <Route exact path='/' element={<Resturants/>}></Route>
-          <Route exact path='/profile' element={<Profile/>}></Route>
-          <Route exact path='/order' element={<OrderPage/>}></Route>
-          <Route exact path='/cart' element={<Cart/>}></Route>
-          <Route exact path='/admin' element={<AdminPage/>}></Route>
-          <Route exact path='/signup' element={<SignupPage/>}></Route>
-      </Routes>
-      
+      <div className='App'>
+        <Header/>
+          <Routes>
+            <Route exact path='/' element={<Resturants/>}></Route>
+            <Route exact path='/profile' element={<Profile/>}></Route>
+            <Route exact path='/order' element={<OrderPage/>}></Route>
+            <Route exact path='/cart' element={<Cart/>}></Route>
+            <Route exact path='/admin' element={<AdminPage/>}></Route>
+            <Route exact path='/signup' element={<SignupPage/>}></Route>
+          </Routes>
       </div>
     </Router>
-  );
+  )
 }
 
 export default App;
--- my-app/src/Components/OrderPage.js
+++ my-app/src/Components/OrderPage.js
@@ -1,4 +1,4 @@
-import logo from '../logo.svg';
+import logo from '../logo.svg'
 import '../styles/OrderPage.css'
 
 function OrderPage() {
