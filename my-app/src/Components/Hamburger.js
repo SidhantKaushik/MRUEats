7ba29eb9012ad36dc171cce8ce95e3e689cabea8
Sidhant Kaushik <59937333+SidhantKaushik@users.noreply.github.com>
2022-11-15 14:47:27 -0700

    Nav Menu complete

---
+++ my-app/src/Components/Hamburger.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+import "../styles/Hamburger.css"
+
+const Hamburger = (props) => {
+    return ( 
+        <div className="hamburger">
+            <div className="burger 1Line"></div>
+            <div className="burger 2Line"></div>
+            <div className="burger 3Line"></div>
+        </div>
+    )
+}
+export default Hamburger;
--- my-app/src/Components/Header.js
+++ my-app/src/Components/Header.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../styles/Header.css'
 import ReactDOM from 'react-dom'
 import { Link } from 'react-router-dom'
+import Hamburger from "./Hamburger"
+
+
+
+
 
 const Header = (props) => {
+    const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+    const toggleHamburger = () =>{
+        setHamburgerOpen(!hamburgerOpen);
+    }
+
     return ( 
-        <div className='header'>
-            <h1>MRU EATS</h1>
-            <ul className = "menu">
+        <>
+        <header>
+            <h1><Link to='/'>MRU EATS</Link></h1>
+
+            <ul className="menu">
                 <li><Link to='/'>Resturants</Link></li>
                 <li><Link to='/profile'>Profile</Link></li>
                 <li><Link to='/order'>OrderPage</Link></li>
@@ -16,7 +29,65 @@ const Header = (props) => {
                 <li><Link to='/signup'>Sign Up</Link></li>
                 <li><Link to='/account'>Account</Link></li>
             </ul>
-        </div>
+            <div className="hamburger" onClick={toggleHamburger}>
+            <Hamburger/>
+            </div>
+            
+            
+            </header>
+            <div className="locationInfo">
+                <p>Current Address: Address</p>
+            </div>
+            <style jsx="true">{`
+                
+                .menu{
+                    display:flex;
+                    flex-wrap: wrap;
+                    float: right;
+                    margin: 0px;
+                    padding: 0px;
+                    overflow: hidden;
+                }
+                .menu li{
+                    list-style-type: none;
+                    padding-right: 10px;
+                }
+                .hamburger{
+                    display: none;
+                    z-index: 6;
+                } 
+                @media (max-width: 767px){
+                  
+                    .hamburger{
+                        z-index: 6;
+                        margin-right: 10px;
+                        align-self: center;
+                        display:flex;
+                        position:fixed;
+                        right: 0;
+                        top: 13px;
+                        width:30px;
+                        height:30px;
+                    }
+                
+                   
+                    .menu{
+                        display: ${hamburgerOpen ? 'flex' : 'none'};
+                        flex-direction: column;
+                        background-color: #033453;
+                        height: fit-content;
+                        font-size: 20px;
+                        width: 100vw;
+                        top: 96px;
+                        right: 0;
+                        justify-content: flex-start;
+                        position: fixed;
+                        
+                    }
+                    
+                }
+            `}</style>
+</>
     )
 }
 export default Header;
