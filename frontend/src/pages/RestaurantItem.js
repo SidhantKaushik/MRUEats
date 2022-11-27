217d11bc97670fdfc7a22a46dfd6e8bb48f596da
Sidhant Kaushik <59937333+SidhantKaushik@users.noreply.github.com>
2022-11-26 23:15:50 -0700

    home page styled

--- frontend/src/pages/RestaurantItem.js
+++ frontend/src/pages/RestaurantItem.js
@@ -7,12 +7,13 @@ const RestaurantItem = (props) => {
     console.log(props)
 
     return (
-        <Link to='/restaurantDetails' state={{id:props.id, name:props.name, address:props.address, close:props.close, open:props.open}}>
+        <Link to='/restaurantDetails' state={{id:props.id, name:props.name, rating:props.rating, address:props.address, close:props.close, open:props.open}}>
         <div className='restaurants'>
             <div className='logo'><img src='https://logos-world.net/wp-content/uploads/2021/08/Chick-fil-A-Logo.png'></img></div>
-            <div className='info'> <h2>{props.name}</h2>
+            <div className='name'> <h2>{props.name}</h2></div>
+            <div className='rating'><p>{props.rating}</p></div>
+            <div className='address'>{props.address}</div>
             <p></p></div>
-        </div>
         </Link>
     )
 }
