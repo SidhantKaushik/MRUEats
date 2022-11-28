a0333f494472561a35bba271a07220335dc7549b
Erika Robles <erikarobles2551@gmail.com>
2022-11-28 13:09:09 -0700

    Attempting POST restaurant logic

--- backend/controllers/restaurantController.js
+++ backend/controllers/restaurantController.js
@@ -20,9 +20,46 @@ const getRestaurantById = asyncHandler(async (req, res) => {
     res.status(200).json(restaurants);
 });
 
+// @desc   Add restaurant 
+// @route  POST /api/restaurants/
+// @access Private
+const addRestaurant = asyncHandler(async (req, res) => {
+    const { logo, name, rating, address, open, close, category } = req.body;
+
+    //Create restaurant
+    const restaurant = await Restaurant.create({
+        id: 1,
+        logo: logo,
+        name: name,
+        rating: rating,
+        address: address,
+        open: open,
+        close: close,
+        category: category
+    });
+
+    if (restaurant) {
+        res.status(201).json({
+            _id: restaurant.id,
+            logo: restaurant.logo,
+            name: restaurant.name,
+            rating: restaurant.rating,
+            address: restaurant.address,
+            open: restaurant.open,
+            close: restaurant.close,
+            category: restaurant.category,
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid restaurant data');
+    }
+    res.status(200);
+});
+
 module.exports = {
 
     getAllRestaurants,
-    getRestaurantById
+    getRestaurantById,
+    addRestaurant,
 
 }
--- backend/routes/restaurantRoutes.js
+++ backend/routes/restaurantRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getAllRestaurants, getRestaurantById } = require('../controllers/restaurantController');
+const { getAllRestaurants, getRestaurantById, addRestaurant } = require('../controllers/restaurantController');
 
 router.get('/', getAllRestaurants);
 router.get('/:id', getRestaurantById);
+router.post('/', addRestaurant);
 
 module.exports = router;
