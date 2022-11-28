23f6fcea50e40140e78e91c94d97cc1ba668b3e7
Sidhant Kaushik <59937333+SidhantKaushik@users.noreply.github.com>
2022-11-27 19:09:14 -0700

    home page mobile friendly

--- frontend/src/pages/RestaurantItem.js
+++ frontend/src/pages/RestaurantItem.js
@@ -8,7 +8,7 @@ const RestaurantItem = (props) => {
 
     return (
         <Link to='/restaurantDetails' state={{id:props.id, name:props.name, rating:props.rating, address:props.address, close:props.close, open:props.open}}>
-        <div className='restaurants'>
+        <div className='restaurantsHomePage'>
             <div className='logo'><img src='https://logos-world.net/wp-content/uploads/2021/08/Chick-fil-A-Logo.png'></img></div>
             <div className='name'> <h2>{props.name}</h2></div>
             <div className='rating'><p>{props.rating}</p></div>
