fb23ae98b0f2b5693c60bdc06bfd57688650d98d
Chris Stepien <31741153+Cstep212@users.noreply.github.com>
2022-12-08 21:45:50 -0700

    Fixed formatting

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -2,8 +2,8 @@
 const NotFound = (props) => {
     return (
         <div>
-            <h1 style={{"padding-left": "1em"}}>Error 404: Not Found</h1>
-            <h2 style={{"padding-left": "1em"}}>The requested URL was not found on this server.</h2>
+            <h1 style={{ "padding-left": "1em" }}>Error 404: Not Found</h1>
+            <h2 style={{ "padding-left": "1em" }}>The requested URL was not found on this server.</h2>
         </div>
     )
 }
