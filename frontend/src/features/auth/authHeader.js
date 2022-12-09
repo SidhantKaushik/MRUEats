33f6ec621eff29fd7312a97c0658c18c781be751
Sidhant Kaushik <59937333+SidhantKaushik@users.noreply.github.com>
2022-12-09 14:03:25 -0700

    clearing

--- frontend/src/features/auth/authHeader.js
+++
@@ -1,9 +0,0 @@
-export default function authHeader() {
-  const user = JSON.parse(localStorage.getItem('user'));
-
-  if (user && user.token) {
-    return { Authorization: 'Bearer ' + user.token };
-  } else {
-    return {};
-  }
-}
