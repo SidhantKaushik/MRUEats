a626a7b591e32f86f97c516767ec09262cdc076a
Sidhant Kaushik <59937333+SidhantKaushik@users.noreply.github.com>
2022-12-09 14:16:53 -0700

    Revert "Merge branch 'main' of https://github.com/SidhantKaushik/MRUEats into develop"
    This reverts commit 5af42380cb392a8f19b31080d74b5b22e5a239dd, reversing
    changes made to 4a92c17d99a5009757a124ad59abb52b1671b5fa.

---
+++ TestingWorkflow.js
@@ -0,0 +1,12 @@
+friends = ['Sidhant' , 'Erika', 'thatotherguy']
+
+friends.forEach(element => {
+    text = Name(element)
+    print(text)
+});
+
+
+function Name (friend) {
+    friend += ' is cool chap'
+    return
+}
